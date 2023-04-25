9103ca75101336b42f0c03c6a4be55b076111ebd
markpaul798 <131008934+markpaul798@users.noreply.github.com>
2023-04-25 10:39:22 +0300

    Create twttr.py

---
+++ twttr.py
@@ -0,0 +1,9 @@
+Def main():
+    Text = input(“Enter some text: “)
+    Vowels = “aeiouAEIOU”
+    For vowel in vowels:
+        Text = text.replace(vowel, “”)
+    Print(text)
+
+If __name__ == “__main__”:
+    Main()
